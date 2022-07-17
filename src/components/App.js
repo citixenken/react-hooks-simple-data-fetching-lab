81ea35e6b49905939f856f0a5456f13121a47668
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-07-17 07:41:09 +0300

    commit tree 75143204269b44acca338fb2e233590caa79b9f6

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [image, setImage] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const DOG_API = "https://dog.ceo/api/breeds/image/random";
+
+  useEffect(() => {
+    fetch(DOG_API)
+      .then((response) => response.json())
+      .then((data) => {
+        setImage(data.message);
+        setLoading(true);
+      });
+  }, []);
+
+  // loading msg
+  if (!loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <img src={image} alt="A Random Dog" />
+    </div>
+  );
+}
+
+export default App;
